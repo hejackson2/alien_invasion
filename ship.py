d466e25ef7a624400c681203238b38e2e770dd2a
Ed Jackson <95002914+hejackson2@users.noreply.github.com>
2024-04-03 19:42:00 -0400

    screen boundry check

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -15,6 +16,9 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flag; start with a ship that is not moving.
         self.moving_right = False
         self.moving_left = False
@@ -22,10 +26,10 @@ class Ship:
 
     def update(self):
         """UPdate the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
 
     
     def blitme(self):
